873079cf7486e4b160d61745d6276b668c8a1932
Hany Raza <37892015+hany110@users.noreply.github.com>
2024-04-09 14:07:48 -0400

    Delete New folder directory

--- New folder/src/Food.java
+++
@@ -1,7 +0,0 @@
-/*
-* Different food types eaten by birds
- */
-public enum Food {
-    BERRIES, SEEDS, FRUIT, INSECTS, OTHER_BIRDS, EGGS, SMALL_MAMMALS, FISH,
-    BUDS, LARVAE, AQUATIC_INVERTEBRATES, NUTS, VEGETATION
-}
